9e4cb343ca56a121b0eabacfe20bb7a1b96addd3
bebelacccda-dev <bebelacccda@gmail.com>
2026-09-19 17:34:51 +0800

    Tambah proyek p01-halo-java dan kelas HaloPBO2

---
+++ p01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/KartuMahasiswa.java
@@ -0,0 +1,35 @@
+
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.uniska.pbo2.p01;
+
+/**
+ *
+ * @author acer
+ */
+public class KartuMahasiswa {
+     public static void main(String[] args){
+        String nama = "Muhammad Rafi'i";
+        String npm = "2410010429";
+        String prodi = "Teknik Informatika";
+        String semester = "5";
+        String alasan = "Bukan kah wajib mengambil kalo tidak tidak bisa lulus";
+        
+        
+        System.out.println("=========================="
+               );
+        System.out.println("Kartu Mahasiswa PBO 2");
+        System.out.println("==========================");
+        System.out.println("Nama      :" + nama);
+        System.out.println("Npm       :" + npm);
+        System.out.println("Prodi     :" + prodi);
+        System.out.println("Semester  :" + semester);
+        System.out.println("Alasan    :" + alasan);
+        System.out.println("==========================");
+     }
+}
